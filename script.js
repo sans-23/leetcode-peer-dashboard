63dff624a580f9a0a5393f5722bf8dd647d76520
Sanskar Jain <sans@Sanskars-MacBook-Air.local>
2024-10-21 21:01:01 +0530

    UI enhancement

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const users = ['sans-23', 'restart24', 'restart2024', 'tushika_', 'restart_ykm_21', 'revanthreddy0612', 'paras_bhai', 'Sonal_Tulasyan', 'chvishal281', 'restart2k24']; // Add more usernames to track here.
 
+const userTab = document.getElementById('user-tab');
+const problemTab = document.getElementById('problem-tab');
+const userList = document.getElementById('user-list');
+const problemList = document.getElementById('problem-list');
+
+// Toggle panels
+userTab.addEventListener('click', () => {
+  userTab.classList.add('active');
+  problemTab.classList.remove('active');
+  userList.classList.add('active');
+  problemList.classList.remove('active');
+});
+
+problemTab.addEventListener('click', () => {
+  problemTab.classList.add('active');
+  userTab.classList.remove('active');
+  problemList.classList.add('active');
+  userList.classList.remove('active');
+});
+
 async function fetchLeetCodeData(username) {
   const response = await fetch(`https://leetcode-api-faisalshohag.vercel.app/${username}`);
   const data = await response.json();
@@ -64,7 +84,7 @@ function createUserListItem(userData, username, rank) {
 
   li.innerHTML = `
     <div class="card-title">
-      <span>${rank}. ${username}</span> 
+      <span class="user-rank">${rank}. ${username}</span> 
       <span class="solved-data">(Solved ${userData.totalSolved} / ${userData.totalQuestions})</span>
     </div>
     <div class="card-subtitle">
@@ -82,16 +102,24 @@ function createUserListItem(userData, username, rank) {
   return li;
 }
 
-// Create problem list from all users' recent submissions
 function createProblemList(recentSubmissions) {
   const problemList = document.getElementById('problem-list');
+
+  // Sort submissions by timestamp in descending order
+  recentSubmissions.sort((a, b) => b.timestamp - a.timestamp);
+
   const allRecentSubmissions = recentSubmissions.map(submission => `
     <li class="submission-item">
       <span>
-      ${new Date(submission.timestamp * 1000).toLocaleString()} - 
-      <strong>${submission.title}</strong> (<i style="font-weight:normal">${submission.username}</i>)
+        ${new Date(submission.timestamp * 1000).toLocaleString()} - 
+        <a href="https://leetcode.com/problems/${submission.slug}" target="_blank" class="problem-link">
+          <strong>${submission.title}</strong>
+        </a>
+        (<i style="font-weight:normal">${submission.username}</i>)
       </span>
-      <span class="status-${submission.statusDisplay.toLowerCase().replace(/ /g, '-')}" >${submission.statusDisplay}</span>
+      <span class="status-${submission.statusDisplay.toLowerCase().replace(/ /g, '-')}" >
+        ${submission.statusDisplay}
+      </span>
     </li>
   `).join('');
   problemList.innerHTML = allRecentSubmissions;
